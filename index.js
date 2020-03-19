cfdda8c49bcfcca72640586bb162fae39dc68f29
Bradley Cooper <bradley.david.cooper@gmail.com>
2020-03-19 15:20:02 -0700

    Version 1.1.0 - Now supporting timestamps with loggerTS

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 const colorize = require('json-colorizer');
-const {
-  blue, green, gray, yellow, red,
-} = require('chalk');
+const chalk = require('chalk');
+
+const timestamp = () => new Date().toISOString();
 
 const colorObj = (txt) => {
   let newTxt = ''; // this means I'm not showing undefineds :/
   if (typeof txt === 'string') {
-    newTxt = txt;
+    newTxt = ` ${txt}`;
   } else if (txt === null) {
-    newTxt = gray(txt);
+    newTxt = ` ${chalk.gray(txt)}`;
   } else if (typeof txt === 'object') {
     newTxt = `\n${colorize(JSON.stringify(txt, null, 2))}`;
   }
   return newTxt;
 };
 
-/* eslint-disable no-console */
-exports.logger = {
-  error: (txt, msg) => console.log(`${red('error: ')}${colorObj(txt)}\n${msg}`),
-  info: (txt, msg) => console.log(`${green('info: ')}${colorObj(txt)}${colorObj(msg)}`),
-  warn: (txt, msg) => console.log(`${yellow('warn: ')}${colorObj(txt)}${colorObj(msg)}`),
-  debug: (txt, msg) => console.log(`${blue('debug: ')}${colorObj(txt)}${colorObj(msg)}`),
+const formatter = (type, color, ts, txt, msg, msgRaw = false) => {
+  /* eslint-disable-next-line no-console */
+  console.log(`${
+    ts ? `${timestamp()} - ` : ''
+  }${
+    chalk[color](`${type}:`)
+  }${
+    colorObj(txt)
+  }${
+    msgRaw ? `\n${msg}` : colorObj(msg)
+  }`);
 };
-/* eslint-enable no-console */
 
+const showRawMsgForError = true;
+const log = (ts = false) => ({
+  error: (txt, msg) => formatter('error', 'red', ts, txt, msg, showRawMsgForError),
+  info: (txt, msg) => formatter('info', 'green', ts, txt, msg),
+  warn: (txt, msg) => formatter('warn', 'yellow', ts, txt, msg),
+  debug: (txt, msg) => formatter('debug', 'blue', ts, txt, msg),
+});
+
+exports.logger = log();
+exports.loggerTS = log(true);
